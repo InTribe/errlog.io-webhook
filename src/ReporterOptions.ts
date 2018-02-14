6856cefb528c489642a6ec3238897f2da53f448b
Aaron Chilcott <aaron.chilcott@gmail.com>
2018-02-15 08:29:16 +1100

    Adds dateformat to report options interface

--- src/ReporterOptions.ts
+++ src/ReporterOptions.ts
@@ -4,5 +4,6 @@ import {HttpRequest} from './HttpRequest';
 export interface IReporterOptions {
   url?: string;
   httpRequest?: HttpRequest;
+  dateFormat?: string;
   errLogIoOptions: IErrLogIoOptions;
 }
